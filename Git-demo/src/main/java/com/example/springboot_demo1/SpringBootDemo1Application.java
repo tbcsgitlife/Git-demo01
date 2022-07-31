cf804b2865f8234ce83e345cbfc06c9d4b7a11d4
tbc <673810598@qq.com>
2022-07-31 16:02:13 +0800

    第一次提交

--- Git-demo/src/main/java/com/example/springboot_demo1/SpringBootDemo1Application.java
+++ Git-demo/src/main/java/com/example/springboot_demo1/SpringBootDemo1Application.java
@@ -4,8 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RestController
 @SpringBootApplication
 @MapperScan("com.example.springboot_demo1.mapper")
 public class SpringBootDemo1Application {
@@ -14,6 +15,10 @@ public class SpringBootDemo1Application {
 
         SpringApplication.run(SpringBootDemo1Application.class, args);
 
+    }
+    @RequestMapping("/")
+    public String index(){
+        return "第二次提交";
     }
 
 }
